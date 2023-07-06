94574ce85b4a44c473f232d61418c9525a482d55
ur5us <juhah@web.de>
2023-07-07 11:22:08 +1200

    Add types, improve error messages and indent

--- src/lib/mapErrorToHumanMessage.ts
+++ src/lib/mapErrorToHumanMessage.ts
@@ -1,18 +1,15 @@
-const errorMap = {
-    "Cannot access video stream (NotAllowedError).": "Can't access video stream, try choosing another camera",
-    "Cannot access video stream (NotReadableError).": "Selected camera busy, try choosing another.",
-    "Cannot access video stream (NotFoundError).": "No Cameras Found.",
-    "Camera Permission denied": "You have denied camera permissions, to use the camera, allow it in your browser settings.",
-    "WebAssembly Not Supported": "Your browser does not support WebAssembly. Please use the latest version of Chrome, Firefox or Safari.",
+const errorMap: { [index: string]: string } = {
+  "Cannot access video stream (NotAllowedError).": "Looks like we don’t have permission to access your camera. Please allow camera access.",
+  "Cannot access video stream (NotReadableError).": "Looks like we can’t get access to the camera. Try choosing another or quit apps that have exclusive access on the camera.",
+  "Cannot access video stream (NotFoundError).": "Looks like your device does not have a camera or your browser can’t find it.",
+  "Camera Permission denied": "Looks like we don’t have permission to access your camera. Please allow camera access.",
+  "WebAssembly Not Supported": "Your browser does not support WebAssembly. Please use the latest version of Chrome, Firefox or Safari.",
 }
 
 export function mediaErrorToMessage(err: Error): string {
-    console.error("Media Error:",err.message)
-    if (errorMap[err.message]) {
-        return errorMap[err.message]
-    } else {
-        return `Unknown Error (${err.message})`
-    }
+  console.error("Media Error:", err.message)
+
+  return errorMap[err.message] ?? `Unknown Error (${err.message})`
 }
 
 
